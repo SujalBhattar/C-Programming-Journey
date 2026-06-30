4702b64bbf38d89a3660389abbbc8184e8639371
SujalBhattar <sbhattar688@gmail.com>
2026-06-30 17:45:55 +0530

    reverse string

---
+++ 06_Strings/string_reverse.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    char str[100], temp;
+    
+    printf("Enter a string: ");
+    fgets(str,100,stdin);
+
+    int i=0;
+
+    while(str[i]!='\0'){
+        i++;
+    }
+
+    // remove newline from fgets
+    str[i-1]='\0';
+    i--;
+
+    for(int j=0; j<i/2; j++){
+        temp=str[j];
+        str[j]=str[i-j-1];
+        str[i-j-1]=temp;
+    }
+
+    printf("Reversed string: %s", str);
+
+    return 0;
+}
